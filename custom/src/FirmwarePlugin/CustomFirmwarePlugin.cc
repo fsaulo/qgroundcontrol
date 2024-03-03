6387cb243bccded9014debe80a4b1a15debacca4
ryanjAA <rjohnston@appliedaeronautics.com>
2024-03-02 18:26:38 -0600

    add VPS to available modes in AAGS

--- custom/src/FirmwarePlugin/CustomFirmwarePlugin.cc
+++ custom/src/FirmwarePlugin/CustomFirmwarePlugin.cc
@@ -20,7 +20,7 @@ CustomFirmwarePlugin::CustomFirmwarePlugin()
     for (int i = 0; i < _flightModeInfoList.count(); i++) {
         FlightModeInfo_t& info = _flightModeInfoList[i];
         //-- Narrow the flight mode options to only these
-        if (info.name != _stabilizedFlightMode && info.name != _posCtlFlightMode && info.name != _holdFlightMode && info.name != _missionFlightMode && info.name != _rtlFlightMode) { //AA - restrict flight modes
+        if (info.name != _stabilizedFlightMode && info.name != _posCtlFlightMode && info.name != _holdFlightMode && info.name != _missionFlightMode && info.name != _vpsFlightMode && info.name != _rtlFlightMode) { //AA - restrict flight modes
             // No other flight modes can be set
             info.canBeSet = false;
         }
